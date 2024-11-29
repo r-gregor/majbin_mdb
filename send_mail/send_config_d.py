6943e9bf32632f7f628c9054e273f4f1dc43927d
r-gregor <gredelonghi@gmail.com>
2024-11-29 16:07:24 +0100

    update 20241129_1607_mdb

---
+++ send_mail/send_config_d.py
@@ -0,0 +1,10 @@
+# define email variables:
+# Header info:
+gFrom = "gredelonghi@yahoo.com"
+gTo = ['gregor.redelonghi@energetika.si']
+gSmtp = "smtp.mail.yahoo.com"
+gPort = 587
+
+# Credentials:
+gUnm="gredelonghi@yahoo.com"
+gPsswd='yszpbytpwobdixah'
