9a5c51b8c8827c4ded93b17256ecb7a1272e99e7
r-gregor <gredelonghi@gmail.com>
2024-12-18 19:49:35 +0100

    update 20241218_1949_mdb

---
+++ send_mail/send_config_d.sh
@@ -0,0 +1,9 @@
+# define email variables:
+# Header info:
+gFrom="gredelonghi@yahoo.com"
+gTo='gregor.redelonghi@energetika.si'
+gSrv="smtp.mail.yahoo.com:587"
+
+# Credentials:
+gUnm="gredelonghi@yahoo.com"
+gPswd='yszpbytpwobdixah'
