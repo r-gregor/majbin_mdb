59c98ca6fd5b8d32cbb73d2e87d410670d7abff7
r-gregor <gredelonghi@gmail.com>
2025-09-30 19:04:26 +0200

    update 20250930_1904_mdb

--- BASH_HACKS.sh
+++ BASH_HACKS.sh
@@ -15,10 +15,31 @@ echo -n 'fname_full:     ${path_full##*\/}     '; echo "${path_full##*\/}"
 echo -n 'fname_base:     ${fname_full%%.*}     '; echo "${fname_full%%.*}"
 echo -n 'fname_ext:      ${fname_full##*.}     '; echo "${fname_full##*.}"
 
-echo
+echo "---"
 echo 'Absolute path to script location -- NOT the current working dir: $(dirname $(realpath ${BASH_SOURCE[0]}))'
 echo "$(dirname $(realpath ${BASH_SOURCE[0]}))"
 
-echo
+echo "---"
+echo "run command from script and detach from shell -- no nohup.out file:"
+echo 'nohup <command> >/dev/null 2>&1 &'
 
+echo "---"
+echo -n "set IFS to newline:    "
+echo "IFS=$'\n'"
+
+echo -n "set IFS to orginal:    "
+echo "IFS=$' \t\n'"
+
+echo "---"
+echo "Read output of command into array:"
+echo 'cmd=$1'
+echo 'output=()'
+echo 'readarray -t -O ${#output[@]} output < <(cmd)'
+echo ""
+echo "Read contents of an array:"
+echo 'for (( i=0; i<${#output[@]}; i++ )); do'
+echo '	echo -e ">\t${output[$i]}"'
+echo 'done'
+
+echo "---"
 
