9b66451dc4196f446d8f7d1eac99522be8c09751
r-gregor <gredelonghi@gmail.com>
2026-08-06 22:47:02 +0200

    update 20260806_2247_mdb

--- isitinsvm.sh
+++ isitinsvm.sh
@@ -4,7 +4,7 @@ PTH="/home/rgregor/majstaf/majmedia/_DSVM.txt"
 
 
 if [ $# -ne 1 ]; then
-	echo -e "[ERROR] must supply a part of movie name\n"
+	printf "[ERROR] must supply a part of movie name\n\n"
 	exit
 else
 	PTRN=$1
@@ -15,4 +15,7 @@ grep -i "$PTRN" $PTH
 if [ $? -ne 0 ]; then
 	echo "NOT IN THE _DSVM: ${PTRN}"
 fi
+
+printf "\n"
+
 
